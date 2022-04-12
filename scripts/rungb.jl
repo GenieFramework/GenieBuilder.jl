ea15ef592887a7e20010901c7a40c07f2b5ef00d
Adrian Salceanu <adrian.salceanu@gmail.com>
2022-04-12 12:31:41 +0300

    Use github repo not dev

--- scripts/rungb.jl
+++ scripts/rungb.jl
@@ -11,11 +11,14 @@ function installgb()
 
   cd(gbdir)
 
-  # `julia -e 'using Pkg;Pkg.activate(".");Pkg.add(url="https://github.com/GenieFramework/GenieBuilder.jl");using GenieBuilder;GenieBuilder.postinstall();'` |> run
   `julia -e 'using Pkg;Pkg.activate(".");
-              Pkg.develop("GenieBuilder");
+              Pkg.add(url="https://github.com/GenieFramework/GenieBuilder.jl");
               using GenieBuilder;
               GenieBuilder.postinstall();'` |> run
+  # `julia -e 'using Pkg;Pkg.activate(".");
+  #             Pkg.develop("GenieBuilder");
+  #             using GenieBuilder;
+  #             GenieBuilder.postinstall();'` |> run
 end
 
 function startgb()
