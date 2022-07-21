b76e78579d4e137b668001f4726dd44e1d56a33b
Daniel Aguilar <dani@protozoo.com>
2022-07-21 14:31:25 +0200

    Bugfix: Save and Rulers icons conflicting

--- public/scripts/nocodeEditorMain.js
+++ public/scripts/nocodeEditorMain.js
@@ -230,6 +230,16 @@ function initNoCodeEditor(){
     active: false,
   });
 
+  editor.Panels.addButton('options', { 
+    attributes: {
+      title: 'Toggle Rulers'
+    },
+    context: 'toggle-rulers', //prevents rulers from being toggled when another views-panel button is clicked 
+    label: `<svg width="18" viewBox="0 0 16 16"><path d="M0 8a.5.5 0 0 1 .5-.5h15a.5.5 0 0 1 0 1H.5A.5.5 0 0 1 0 8z"/><path d="M4 3h8a1 1 0 0 1 1 1v2.5h1V4a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2v2.5h1V4a1 1 0 0 1 1-1zM3 9.5H2V12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V9.5h-1V12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V9.5z"/></svg>`,
+    command: 'ruler-visibility',
+    id: 'ruler-visibility'
+  });
+
   editor.Panels.addButton('options', {  // Fist argument is part of container element's class (i.e. "gjs-pn-options")
     //id: 'myNewButton',
     //label: 'Gn',
@@ -254,16 +264,6 @@ function initNoCodeEditor(){
       id: "open-code"
     }
   ]);
-
-  editor.Panels.addButton('options', { 
-    attributes: {
-      title: 'Toggle Rulers'
-    },
-    context: 'toggle-rulers', //prevents rulers from being toggled when another views-panel button is clicked 
-    label: `<svg width="18" viewBox="0 0 16 16"><path d="M0 8a.5.5 0 0 1 .5-.5h15a.5.5 0 0 1 0 1H.5A.5.5 0 0 1 0 8z"/><path d="M4 3h8a1 1 0 0 1 1 1v2.5h1V4a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2v2.5h1V4a1 1 0 0 1 1-1zM3 9.5H2V12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V9.5h-1V12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V9.5z"/></svg>`,
-    command: 'ruler-visibility',
-    id: 'ruler-visibility'
-  });
 
 
 
