8344f63f11918316a389ce25f320af9ae5516edc
Adrian Salceanu <adrian.salceanu@gmail.com>
2023-02-16 13:12:26 +0100

    refactor GC integration

--- app/resources/applications/Integrations.jl
+++ app/resources/applications/Integrations.jl
@@ -1,20 +1,5 @@
 module Integrations
 
-module GenieCloud
-
-using HTTP
-using JSON3
-using SearchLight
-
-import GenieBuilder
-
-# this wraps the whole module body!
-if haskey(ENV, "GC_API_ENDPOINT") && haskey(ENV, "GC_API_TOKEN")
-  include("integrations/geniecloud.jl")
-else
-  include("integrations/geniecloud_disabled.jl")
-end # end if
-
-end # GenieCloud
+include("integrations/GenieCloud.jl")
 
 end
