894924c775817e81b5c3d43cf617ba2b47c1b309
Adrian Salceanu <adrian.salceanu@gmail.com>
2023-02-16 12:45:02 +0100

    integrations refactoring

--- app/resources/applications/Integrations.jl
+++ app/resources/applications/Integrations.jl
@@ -12,20 +12,7 @@ import GenieBuilder
 if haskey(ENV, "GC_API_ENDPOINT") && haskey(ENV, "GC_API_TOKEN")
   include("integrations/geniecloud.jl")
 else
-  function init()::Nothing
-    @debug "GenieCloud integration is not enabled.
-    Please set the GC_API_ENDPOINT and GC_API_TOKEN environment variables."
-
-    nothing
-  end
-
-  function updateapp(app, delay = 0)::Nothing
-    nothing
-  end
-
-  function container_idle()::Nothing
-    nothing
-  end
+  include("integrations/geniecloud_disabled.jl")
 end # end if
 
 end # GenieCloud
---
+++ app/resources/applications/integrations/geniecloud_disabled.jl
@@ -0,0 +1,14 @@
+function init()::Nothing
+  @debug "GenieCloud integration is not enabled.
+  Please set the GC_API_ENDPOINT and GC_API_TOKEN environment variables."
+
+  nothing
+end
+
+function updateapp(app, delay = 0)::Nothing
+  nothing
+end
+
+function container_idle()::Nothing
+  nothing
+end
