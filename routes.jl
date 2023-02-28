32117bfcb82c26d706338ec6b7e2e74a34958981
abhimanyuaryan <8083613+AbhimanyuAryan@users.noreply.github.com>
2023-02-28 19:46:28 +0530

    disabling gc globally

--- routes.jl
+++ routes.jl
@@ -3,6 +3,9 @@ using GenieBuilder
 using GenieBuilder.ApplicationsController
 using GenieBuilder.UsersController
 using RemoteREPL
+using Pkg
+
+Pkg._auto_gc_enabled[] = false;
 
 Genie.config.websockets_server = true
 
