59c4188c48561bf9ab143af3eee60b1eb9b94d48
Adrian Salceanu <adrian.salceanu@gmail.com>
2023-03-30 13:21:54 +0200

    missing api version

--- app/resources/applications/integrations/GenieCloud.jl
+++ app/resources/applications/integrations/GenieCloud.jl
@@ -9,9 +9,10 @@ import GenieBuilder
 function __init__()
   @static if haskey(ENV, "GC_API_ENDPOINT") && haskey(ENV, "GC_API_TOKEN")
     @eval begin
-      const GC_API_ENDPOINT_USERS = ENV["GC_API_ENDPOINT"] * "/api/v1"
-      const GC_API_ENDPOINT_APPS = ENV["GC_API_ENDPOINT"] * "/apps"
-      const GC_API_ENDPOINT_CONTAINERS = ENV["GC_API_ENDPOINT"] * "/containers"
+      const GC_API_VERSION = "/api/v1"
+      const GC_API_ENDPOINT_USERS = ENV["GC_API_ENDPOINT"] * "/$GC_API_VERSION"
+      const GC_API_ENDPOINT_APPS = ENV["GC_API_ENDPOINT"] * "/$GC_API_VERSION" * "/apps"
+      const GC_API_ENDPOINT_CONTAINERS = ENV["GC_API_ENDPOINT"] * "/$GC_API_VERSION" * "/containers"
       const GC_API_HEADERS = [
         "Authorization" => "bearer $(ENV["GC_API_TOKEN"])",
         "Content-Type" => "application/json",
