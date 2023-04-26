8050a0e5da8b9cdbb50e3977c5fd9b2852f6bf8c
Daniel Aguilar <dani@protozoo.com>
2023-04-26 16:26:36 +0200

    Avoid no-code reload on save; no-code editor won't load app scripts anymore (deps/Scripts).

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -72,7 +72,7 @@ window.onload = () => {
       ];
       // Define a list of libraries/Assets not needed in the editor context
       // and avoid loading them to optimise newtwork/memory/cpu resources
-      const blackList = [ "plotly", "quasar", "vueresize", "vueplotly"/* , "autoreload" */ ];
+      /* const blackList = [ "plotly", "quasar", "vueresize", "vueplotly" ];
       deps.scripts.forEach( (item)=>{
          //appConfig.contentScripts.push( appConfig.url + item );
          const scriptPath = appConfig.url + item;
@@ -89,7 +89,7 @@ window.onload = () => {
               myScript.setAttribute("src", scriptPath);
               document.body.appendChild(myScript);              
             }
-        } );
+        } ); */
         
         return ApiConnector.readFileContents( filePath );
     })
