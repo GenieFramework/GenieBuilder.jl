12c2fa5e6d86915fa7173e703f497ac155cde95c
Daniel Aguilar <dani@protozoo.com>
2023-11-10 13:45:31 +0100

    Add new no-code app (p42)

--- public/apiMock/test-1.jl
+++
@@ -1,7 +0,0 @@
-# These are the contents of file "test-1.jl"
-# =============================================
-
-# function to calculate the volume of a sphere
-function dummt_function(r)
-    return true
-    end
