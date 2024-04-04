f9c9315ec944c29ed31020312287b063a51f8c15
Adrian Salceanu <adrian@genieframework.com>
2024-04-04 15:23:30 +0200

    Fix logging initializer bug

--- config/initializers/logging.jl
+++ config/initializers/logging.jl
@@ -1,8 +1,8 @@
 import Genie
 
 log_path, log_name = if haskey(ENV, "GB_LOG_FILE") && ispath(dirname(ENV["GB_LOG_FILE"]))
-    (dirname(ENV["GB_LOG_FILE"]), basename(ENV["GB_LOG_FILE"]))
     ENV["GB_HEADLESS"] = "true"
+    (dirname(ENV["GB_LOG_FILE"]), basename(ENV["GB_LOG_FILE"]))
 else
     Genie.config.path_log, Genie.Logger.default_log_name()
 end
