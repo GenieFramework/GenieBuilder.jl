e0227922f5232ae7a41d16d8012594db91067f76
Adrian Salceanu <adrian@genieframework.com>
2024-05-09 18:58:13 +0200

    Host for win

--- config/env/prod.jl
+++ config/env/prod.jl
@@ -3,7 +3,7 @@ using Genie, Logging
 Genie.Configuration.config!(
   server_port                     = 10101,
   websockets_port                 = 10101,
-  server_host                     = "0.0.0.0",
+  server_host                     = (Sys.iswindows() ? "127.0.0.1" : "0.0.0.0"),
   log_level                       = Logging.Error,
   log_to_file                     = true,
   server_handle_static_files      = true, # for best performance set up Nginx or Apache web proxies and set this to false
